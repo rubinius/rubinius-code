8429f80e5edfdfdd49c975e040be0360ba401b2c
Ahmad Sherif <me@ahmadsherif.com>
2016-02-26 21:14:24 +0200

    Include filename in compiler eval cache key

--- lib/rubinius/code/compiler/compiler.rb
+++ lib/rubinius/code/compiler/compiler.rb
@@ -286,7 +286,7 @@ module CodeTools
     def self.compile_eval(string, variable_scope, file="(eval)", line=1)
       if ec = @eval_cache
         layout = variable_scope.local_layout
-        if code = ec.retrieve([string, layout, line])
+        if code = ec.retrieve([string, layout, file, line])
           return code
         end
       end
@@ -305,7 +305,7 @@ module CodeTools
       code.add_metadata :for_eval, true
 
       if ec and parser.should_cache?
-        ec.set([string.dup, layout, line], code)
+        ec.set([string.dup, layout, file, line], code)
       end
 
       return code
