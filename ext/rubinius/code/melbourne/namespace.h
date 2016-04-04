f0534d793e8fbdcfd66ddd7834c6f4ad7e7b85b7
Brian Shirai <brixen@gmail.com>
2016-04-03 22:48:24 -0700

    Some misc cleanup from import.

--- ext/rubinius/code/melbourne/namespace.h
+++
@@ -1,3 +0,0 @@
-#define MELBOURNE                 melbourne_1_2_0_0
-#define MELBOURNE_FILE_TO_AST     melbourne_1_2_0_0_file_to_ast
-#define MELBOURNE_STRING_TO_AST   melbourne_1_2_0_0_string_to_ast
